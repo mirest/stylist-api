2db13438164ac00ff8d3c240242cc0557c3baa9b
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-04-02 09:57:49 +0300

    ch(model): create user model
    - add model fields
    - add test for the model
    
    [Starts #165026611]

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from datetime import datetime, timedelta
 
-# Create your models here.
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
+from .base_model import BaseModel
+from django.core.validators import RegexValidator
+
+
+class UserManager(BaseUserManager, object):
+
+    def create_user(self, password=None):
+        if self.phone_number is None:
+            raise TypeError('Users must have a phone number.')
+
+        if self.email is None:
+            raise TypeError('Users must have an email address.')
+        self.email = self.normalize_email(self.email)
+        self.set_password(password)
+        self.save()
+        return self
+
+    def create_superuser(self, password=None):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel, UserManager):
+    email = models.EmailField(unique=True, null=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,20}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(
+        db_index=True, unique=True, null=False, blank=False, validators=[phone_regex], max_length=20)
+
+    is_active = models.BooleanField(default=True)
+
+    is_verified = models.BooleanField(default=False)
+
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ('email',)
+
+    def __str__(self):
+
+        return self.email
+
+    @property
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_email(self):
+        return self.email
---
+++ tests/models/test_user.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from ..factories.users import UserFactory
+from apps.users.models import User
+
+class TestUserModel(TestCase):
+    
+    def test_user_model_saves_data_successfully(self):
+
+        user=UserFactory(email="kimbsimon2@gmail.com",phone_number='+1-224-976-4509')
+        user.create_user()
+        self.assertIn('id',user.__dict__)
+        self.assertEqual('kimbsimon2@gmail.com',user.email)
+        self.assertEqual('kimbsimon2@gmail.com',user.get_email())
+        self.assertEqual('+1-224-976-4509',user.get_phone_number)
+    
+    def test_create_user_without_email(self):
+        user=UserFactory(email=None)
+        with self.assertRaises(TypeError):
+            user.create_user()
+    
+    def test_create_user_without_phone_number(self):
+        user=UserFactory(phone_number=None)
+        with self.assertRaises(TypeError):
+            user.create_user()
+    
+    def test_create_super_user_succeds(self):
+        user=UserFactory(phone_number='+1-224-976-4509')
+        user.create_superuser(password="qwertrewqert")
+        self.assertIn('id',user.__dict__)
+        self.assertEqual(True, user.is_superuser)
+
+    def test_create_super_user_with_no_password_fails(self):
+        user=UserFactory()
+        with self.assertRaises(TypeError):
+            user.create_superuser()
